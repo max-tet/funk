57705311719cdb2c508b698ed7d7e19a162c7551
Max von Tettenborn <contact@max-tettenborn.de>
2017-03-18 11:37:43 +0100

    adding nodes

--- funk/static/js/funk.js
+++ funk/static/js/funk.js
@@ -243,6 +243,7 @@ var load_graph = function () {
 
 var funk_init = function (containerId) {
     init_typeahead();
+    funkInstance.containerId = containerId;
     funkInstance.jsPlumbInstance = getInstance(containerId);
     var pathname = window.location.pathname;
     funkInstance.graphname = pathname.split('/')[2];
@@ -272,12 +273,25 @@ function init_typeahead() {
                 return '<div style="background-color: '+nodeType.color+'">'+nodeType.type+'</div>';
             }
         }
+    })
+    .bind('typeahead:select', function(ev, suggestion) {
+        var nodeId = suggestion.type + '_' + randomString(6);
+        while ($('#'+funkInstance.containerId).find('.funk-node').is('#'+nodeId)) {
+            nodeId = suggestion.type + '_' + randomString(6);
+        }
+        var nodeName = suggestion.type;
+        addNode(funkInstance.jsPlumbInstance, nodeId, nodeName, suggestion, ['5em', '5em']);
+        $('#funk-add-node').find('input').typeahead('val', '');
     });
 }
 
 function shadeColor(color, percent) {   
     var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
     return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+}
+
+function randomString(length) {
+    return (Math.random()+1).toString(36).substr(2,length);
 }
 
 buttonSave = new Vue({
