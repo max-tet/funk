39ba83a896d6d6ae60cd105018e5094b8d222a38
Max von Tettenborn <contact@max-tettenborn.de>
2017-06-26 10:51:14 +0200

    docstring for nodetypes

--- funk/app.py
+++ funk/app.py
@@ -69,6 +69,57 @@ def get_dynamic_types(search_term):
 
 @app.route('/api/nodetypes')
 def types():
+    """
+    Returns a list of available datatypes and nodetypes in json format.
+    The top-level json element is an object and it contains two elements:
+    *datatypes* and *nodetypes*. Both are lists.
+
+    A datatype is an object like this::
+
+        {
+          "id": "boolean",  # snake-case identifier for referencing
+          "name": "Boolean",  # Normal Case name for display in UI
+          "color": "#D66326"  # Color of connectors and connections
+        }
+
+    A nodetype is an object like this::
+
+        {
+            "type": "some_nodetype",  # snake-case identifier for referencing
+            "name": "Some Nodetype",  # Normal Case name that is displayed in small font beneath the node's name
+            "defaultNodeName": "Some Special Nodetype",  # Name for a newly instantiated node, defaults to *name* if omitted
+            "isAbstract": true,  # If true, the nodetype is not listed and can therefore not be instantiated
+            "isStatic": true,  # If true, editing of the node is not allowed
+            "color": "#A58DD2",  # Color of the node
+            "categories": ["Example", "Static"],  # List of category strings under which the UI displays the node
+            "connector_l": [  # List of connectors on the node's left side
+                {
+                    "id": "in",  # snake-case identifier for referencing; must be unique in the scope of the node
+                    "name": "In",  # Normal Case name for display
+                    "type": "string",  # Connector's datatype; must reference an existing datatype from the previous list
+                    "direction": "in"  # Direction of information flow; can be *in* or *out*
+                }
+            ],
+            "connector_r": [  # List of connectors on the node's left side, see *connector_l* for reference on the fields
+                {
+                    "id": "out",
+                    "name": "Out",
+                    "type": "resource",
+                    "direction": "out"
+                }
+            ],
+            "props": [  # List of properties for the user to edit via the UI
+                {
+                    "propid": "value",  # snake-case identifier for referencing; must be unique in the scope of the node
+                    "name": "Value",  # Normal Case name for display
+                    "type": "string",  # Prop's datatype; must reference an existing datatype from the previous list
+                    "value": ""  # Default value
+                }
+            ]
+        }
+
+    :return: Json object containing datatypes and nodetypes.
+    """
     return nodetypes.from_static('nodetypes.js')
 
 
